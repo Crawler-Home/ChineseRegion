9801a6eb96acb78f21782fa0335f98607005edb9
zhulin3141 <zhulin3141@gmail.com>
2016-12-23 17:39:00 +0800

    调整采集逻辑

--- ChineseRegion/spiders/region_spider.py
+++ ChineseRegion/spiders/region_spider.py
@@ -1,28 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-from scrapy.spiders import Spider,Rule
+from scrapy.spiders import CrawlSpider,Rule
 from scrapy.linkextractors import LinkExtractor
 from ChineseRegion.items import ChineseregionItem
 
 
-class RegionSpider(Spider):
+class RegionSpider(CrawlSpider):
     name = "region"
-    #allowed_domains = ["http://www.stats.gov.cn"]
+    allowed_domains = ["stats.gov.cn"]
     start_urls = [
         "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2015/index.html",
     ]
-    # rules = [
-    #     Rule(LinkExtractor(allow=(r'\d+\.html')), 'parse_item')
-    # ]
+    rules = [
+        Rule(LinkExtractor( allow=(r'\d+\.html')), callback='parse_item', follow=False )
+    ]
 
     def parse_item(self, response):
-        print response.body
-
-    def parse(self, response):
+        print '***************' + response.url + '****************************'
         for links in response.css('.provincetr td a'):
-            item = ChineseregionItem()
-            item['code'] = links.xpath('@href').extract()[0].rstrip('.html')
-            item['name'] = links.xpath('text()').extract()[0]
-            item['pcode'] = ''
-            yield item
+            print links.xpath('@href').extract()[0].rstrip('.html')
+            print links.xpath('text()').extract()[0]
+            # item = ChineseregionItem()
+            # item['code'] = links.xpath('@href').extract()[0].rstrip('.html')
+            # item['name'] = links.xpath('text()').extract()[0]
+            # item['pcode'] = ''
+            # yield item
+
+        for city in response.css('.citytr'):
+            print city.xpath('td[1]/a/text()').extract()[0]
+            print city.xpath('td[2]/a/text()').extract()[0]
+
+        for county in response.css('.countytr'):
+            print county.xpath('td[1]/a/text()').extract()[0]
+            print county.xpath('td[2]/a/text()').extract()[0]
